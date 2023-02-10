63fbd6950cc78b2f0e733208b2eb7308d4b3a24d
Aurelija Petrauskaite <aurelija35@gmail.com>
2023-02-10 16:53:07 +0200

    Django, pirma dalis

--- api_example/postit_api/serializers.py
+++ api_example/postit_api/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
 from .models import Post, Comment, CommentLike, PostLike
 
+# class PostSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Post
+#         fields = ['id', 'user', 'title', 'body', 'created']
+
+# pakoregavimas, kad galetu post rasyt tik savo vardu.
 class PostSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+    user_id = serializers.ReadOnlyField(source='user.id')
+
     class Meta:
         model = Post
-        fields = ['id', 'user', 'title', 'body', 'created']
+        fields = ['id', 'user', 'user_id', 'title', 'body', 'created']
--- api_example/postit_api/views.py
+++ api_example/postit_api/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Post, PostLike, Comment, CommentLike
 from .serializers import PostSerializer
 
 # Create your views here.
-class PostList(generics.ListAPIView):
+# class PostList(generics.ListAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+
+# class PostList(generics.ListCreateAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+#
+# def perform_create(self, serializer):
+#     serializer.save(user=self.request.user)
+
+
+class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
